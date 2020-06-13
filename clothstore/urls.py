b106f3e818647a4783df17d0b6c197502fc26cd7
Francis Xavier <njokuchimauche@gmail.com>
2020-06-13 15:12:11 +0100

    Update url

--- clothstore/urls.py
+++ clothstore/urls.py
@@ -1,3 +1,4 @@
+from products import views
 """clothstore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
 ]
