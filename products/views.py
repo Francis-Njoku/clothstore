f90e04b871ca8b5312f8adc37b1037bbe4c95797
Francis Xavier <njokuchimauche@gmail.com>
2020-06-14 12:37:48 +0100

    update views for dynamic content

--- products/views.py
+++ products/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    if request.user.is_authenticated:
+        username_is = "Francis Xavier using context"
+        context = {"username_is": username_is}
+    else:
+        context = {"username_is": "Unknown"}
     template = 'base.html'
-    context = locals()
     return render(request, template, context)
