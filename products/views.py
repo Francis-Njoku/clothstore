3bcebf9f41599421512e152081f6136e66e67601
Francis Xavier <njokuchimauche@gmail.com>
2020-06-14 11:39:28 +0100

    Update base.html

--- products/views.py
+++ products/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    template = 'home.html'
+    template = 'base.html'
     context = locals()
     return render(request, template, context)
