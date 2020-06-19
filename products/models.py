7a0ba9266092fffbfe56d288061209b141f68834
Francis Xavier <njokuchimauche@gmail.com>
2020-06-19 11:11:03 +0100

    Update models

--- products/models.py
+++ products/models.py
@@ -5,14 +5,24 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=50)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=50, null=True, blank=True)
     slug = models.SlugField()
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     active = models.BooleanField(default=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     def get_price(self):
         return self.price
-1
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/images')
+    featured = models.BooleanField(default=False)
+    thumbnail = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.product.title
