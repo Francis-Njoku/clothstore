d2b1a9a17dfb5392bf35fccf33fae2331e912e9a
Francis Xavier <njokuchimauche@gmail.com>
2020-07-19 13:48:41 +0100

    Add cart item model to cart

--- carts/models.py
+++ carts/models.py
@@ -3,7 +3,18 @@ from django.db import models
 # Create your models here.
 from products.models import Product
 
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    update = models.DateTimeField(auto_now_add=False, auto_now=True)
+    
+
+    def __unicode__(self):
+        return self.product.title
+
 class Cart(models.Model):
+    items = models.ManyToManyField(CartItem, null=True, blank=True)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
