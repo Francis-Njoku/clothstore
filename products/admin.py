f94122a59e8ea7142aeb8d0a88253744130e31f8
Francis Xavier <njokuchimauche@gmail.com>
2020-07-27 20:42:52 +0100

    Update admin with function

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, ProductImage
+from .models import Product, ProductImage, Variation
 
 class ProductAdmin(admin.ModelAdmin):
     date_hierarchy = 'timestamp'
@@ -17,3 +17,5 @@ class ProductAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 
 admin.site.register(ProductImage)
+
+admin.site.register(Variation)
