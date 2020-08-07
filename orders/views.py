3642228c24be053ae3fcbcccb455d39cfc40b957
Francis Xavier <njokuchimauche@gmail.com>
2020-08-07 12:40:36 +0100

    Update order views with order details

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
-
+import time
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+
+from carts.models import Cart
+from .models import Order
+
+def checkout(request):
+    try:
+        the_id = request.session['cart_id']
+        cart = Cart.objects.get(id=the_id)
+        print(cart)
+    except:
+        the_id = None    
+        return HttpResponseRedirect(reverse("cart"))
+
+    new_order, created = Order.objects.get_or_create(cart=cart)
+    if created:
+        # Assign a user to the order
+        # assign address
+        new_order.order_id = str(time.time())
+        new_order.save()    
+
+    # run credit card
+    if new_order.status == "Finished":
+        del request.session['cart_id']
+        del request.session['items_total'] 
+        return HttpResponseRedirect(reverse("cart"))  
+
+    context = {}
+    template = "products/home.html"
+    return render(request, template, context)
