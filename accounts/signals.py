9c43a9d2232ab131027e43bccdf5ac720aa885b1
Francis Xavier <njokuchimauche@gmail.com>
2020-08-23 22:11:51 +0100

    Update signals

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,15 +1,34 @@
 import stripe
+import random
+import hashlib
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save
 
-from .models import UserStripe
+from .models import UserStripe, EmailConfirmed
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-User = get_user_model
+User = get_user_model()
+
+
+'''def get_or_create_stripe(sender, user, *args, **kwargs):
+    try:
+        user.userstripe.stripe_id
+    except UserStripe.DoesNotExist:
+        customer = stripe.Customer.create(
+            email = str(user.email)
+        )
+        new_user_stripe = UserStripe.objects.create(
+            user=user,
+            stripe_id = customer.id
+        )
+    except:
+        pass
+
+user_logged_in.connect(get_or_create_stripe) '''
 
 
 # If we decide to change how we want to add the stripe id, this is where we set it for a user_logged_in
@@ -30,33 +49,14 @@ User = get_user_model
         )   
     except:
         pass  '''  
-    
+      
 def get_create_stripe(user):
-    '''try:
-        user.userstripe.stripe_id
-    except UserStripe.DoesNotExist:
-        customer = stripe.Customer.create(
-            email = str(user.email)
-        )    
-        new_user_stripe = UserStripe.objects.create(
-            user=user,
-            stripe_id=customer.id
-        )   
-    except:
-        pass'''
     new_user_stripe, created = UserStripe.objects.get_or_create(user=user)
     if created:
         customer = stripe.Customer.create(
             email = str(user.email)
         )
-        if customer:
-            print('Exist')
-        else:
-            print('Does not exist')    
         new_user_stripe.stripe_id = customer.id
-        #snew_user_stripe.user = user
-        #print(user)
-        #print(customer.id)
         new_user_stripe.save()
 
 
@@ -64,10 +64,13 @@ def user_created(sender, instance, created, *args, **kwargs):
     user = instance
     if created:
         get_create_stripe(user)
-        # send our email
-        #
-    print(sender)
-    print(instance)
-    print(created)
+        email_confirmed, email_is_created = EmailConfirmed.objects.get_or_create(user=user)
+        if email_is_created:
+            short_hash = hashlib.sha1(str(random.random()).encode('utf-8')).hexdigest()[:5]
+            base, domain =str(user.email).split("@")
+            activation_key =  hashlib.sha1((short_hash+base).encode('utf-8')).hexdigest()
+            email_confirmed.activation_key = activation_key
+            email_confirmed.save()
+            email_confirmed.active_user_email()
 
 post_save.connect(user_created, sender=User)
