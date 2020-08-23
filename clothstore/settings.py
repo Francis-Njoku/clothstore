c9a7384e38f7f680d8cbef4178b67b048e37aebe
Francis Xavier <njokuchimauche@gmail.com>
2020-08-23 22:12:30 +0100

    add email SMTP to settings

--- clothstore/settings.py
+++ clothstore/settings.py
@@ -27,7 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DEFAULT_FROM_EMAIL = "Chimauche Njoku <njokuchimauche@gmail.com>"
 
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "njokuchimauche@gmail.com"
+EMAIL_HOST_PASSWORD = "password"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,7 +46,8 @@ INSTALLED_APPS = [
     'products',
     'carts',
     'orders',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    
 ]
 
 MIDDLEWARE = [
