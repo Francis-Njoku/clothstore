98d8b66c125102e30a946146702c772cfc49e1da
Francis Xavier <njokuchimauche@gmail.com>
2020-09-16 01:33:05 +0100

    update marketing view

--- marketing/views.py
+++ marketing/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, HttpResponse, Http404
+from django.utils import timezone
+import datetime
 
 # Create your views here.
+def dismiss_marketing_message(request):
+    print(timezone.now())
+    print('chima ')
+    print(timezone.now() + datetime.timedelta(seconds=8))
+    if request.is_ajax():
+        data = {"success": True}
+        print(data)
+        json_data = json.dumps(data)
+        request.session['dismiss_message_for'] = str(timezone.now() + datetime.timedelta(seconds=8))
+        print(json_data)
+        return HttpResponse(json_data, content_type='application/json')
+    else:
+        raise Http404
+
